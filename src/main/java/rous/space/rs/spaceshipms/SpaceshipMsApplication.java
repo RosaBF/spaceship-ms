c79fbda25a404f15edf4e76e85714204ba5c977b
Rosa Berned <rosaberned@MacBook-Pro-de-Rosa.local>
2024-10-19 17:58:40 +0200

    First commit

---
+++ src/main/java/rous/space/rs/spaceshipms/SpaceshipMsApplication.java
@@ -0,0 +1,13 @@
+package rous.space.rs.spaceshipms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpaceshipMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpaceshipMsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/rous/space/rs/spaceshipms/SpaceshipMsApplicationTests.java
@@ -0,0 +1,13 @@
+package rous.space.rs.spaceshipms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpaceshipMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
